48132bd79ce8bd5ef7bdfbf36a62cba0e14c9b97
Guillem <guillem.dardill@gmail.com>
2020-12-09 12:18:17 +0100

    google TTS init

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,6 +1,11 @@
 const { findAllCampaigns } = require('../models/campaigns');
+const quickStart = require('../services/textToSpeech');
 
 module.exports.getCollection = async (req, res) => {
   const [data] = await findAllCampaigns();
   res.json(data);
+};
+
+module.exports.vocalization = async () => {
+  quickStart();
 };

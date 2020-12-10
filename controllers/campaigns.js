4dba3e20ee15b4b2881b12dbcda00e483daba130
Twiggui <guillem.dardill@gmail.com>
2020-12-10 11:29:17 +0100

    text vocalization and audio play test working
    Text vocalization and audio play test working

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,6 +1,6 @@
+const fs = require("fs")
 const { findAllCampaigns } = require('../models/campaigns');
 const quickStart = require('../services/textToSpeech');
-const fs = require("fs")
 
 module.exports.getCollection = async (req, res) => {
   const [data] = await findAllCampaigns();
@@ -8,13 +8,13 @@ module.exports.getCollection = async (req, res) => {
 };
 
 module.exports.vocalization = async (req, res) => {
-  let fileName = await quickStart(req.body.message);
+  const fileName = await quickStart(req.body.message);
 res.status(200).send(fileName)
 };
 
 module.exports.playAudio = async (req, res) => {
 const audioFile = `${req.query.audio}.mp3`
 // res.type = path.extname(audio);
-let stream = fs.createReadStream('/usr/src/app/file-storage/public/' + audioFile);
+const stream = fs.createReadStream(`/usr/src/app/file-storage/public/${  audioFile}`);
 stream.pipe(res);
 };

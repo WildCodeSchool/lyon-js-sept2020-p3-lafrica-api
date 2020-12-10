c5e530fc5f58031856bd16d722ddbc3a8b881bf2
Jean Ducret <jean.dcrt@gmail.com>
2020-12-10 13:59:22 +0100

    update dynamic path

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -1,4 +1,5 @@
-const fs = require("fs")
+const fs = require('fs');
+const path = require('path');
 const { findAllCampaigns } = require('../models/campaigns');
 const quickStart = require('../services/textToSpeech');
 
@@ -9,12 +10,13 @@ module.exports.getCollection = async (req, res) => {
 
 module.exports.vocalization = async (req, res) => {
   const fileName = await quickStart(req.body.message);
-res.status(200).send(fileName)
+  res.status(200).send(fileName);
 };
 
 module.exports.playAudio = async (req, res) => {
-const audioFile = `${req.query.audio}.mp3`
-// res.type = path.extname(audio);
-const stream = fs.createReadStream(`/usr/src/app/file-storage/public/${  audioFile}`);
-stream.pipe(res);
+  const audioFile = `${req.query.audio}.mp3`;
+  // res.type = path.extname(audio);
+  const pathFile = path.join(__dirname + '/../file-storage/public');
+  const stream = fs.createReadStream(`${pathFile}/${audioFile}`);
+  stream.pipe(res);
 };
--- services/textToSpeech.js
+++ services/textToSpeech.js
@@ -5,6 +5,7 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 // Import other required libraries
 const fs = require('fs');
 const util = require('util');
+const path = require('path');
 
 // const projectId = 'test-lam'; S
 // const keyFileName = '../Test-LAM-0d28b0da2eea.json';
@@ -20,18 +21,31 @@ async function quickStart(textToVocalize) {
   const request = {
     input: { text },
     // Select the language and SSML voice gender (optional)
-    voice: { languageCode: 'fr-FR', ssmlGender: 'FEMALE', name : "fr-FR-Wavenet-E" },
+    voice: {
+      languageCode: 'fr-FR',
+      ssmlGender: 'FEMALE',
+      name: 'fr-FR-Wavenet-E',
+    },
     // select the type of audio encoding
-    
+
     audioConfig: { audioEncoding: 'MP3' },
   };
 
   // Performs the text-to-speech request
   const [response] = await client.synthesizeSpeech(request);
+  const pathFile = path.join(__dirname + '/../file-storage/public');
+
   // Write the binary audio content to a local file
   const writeFile = util.promisify(fs.writeFile);
-  const audioFileName = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 5)
-  await writeFile(`/usr/src/app/file-storage/public/${audioFileName}.mp3`, response.audioContent, 'binary');
+  const audioFileName = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substring(0, 5);
+  await writeFile(
+    `${pathFile}/${audioFileName}.mp3`,
+    response.audioContent,
+    'binary'
+  );
   console.log(`Audio content written to file: ${audioFileName}.mp3`);
   return audioFileName;
 }

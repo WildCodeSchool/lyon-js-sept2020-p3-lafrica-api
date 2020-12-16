6517fd7f1802ea48c3d9b2336858eed83510d36c
Jean Ducret <jean.dcrt@gmail.com>
2020-12-16 14:38:10 +0100

    update variables with environnment

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
-const session = require("express-session");
-const { inTestEnv, inProdEnv, SERVER_PORT } = require("./env");
-const sessionStore = require("./sessionStore");
+const express = require('express');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const session = require('express-session');
+const {
+  inTestEnv,
+  inProdEnv,
+  SERVER_PORT,
+  SESSION_COOKIE_NAME,
+  SESSION_COOKIE_SECRET,
+  CORS_ALLOWED_ORIGINS,
+} = require('./env');
+const sessionStore = require('./sessionStore');
 
 const app = express();
 
 // docs
 if (!inProdEnv && !inTestEnv) {
-  const swaggerDocument = YAML.load("./docs/swagger.yaml");
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  const swaggerDocument = YAML.load('./docs/swagger.yaml');
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 // pre-route middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: CORS_ALLOWED_ORIGINS,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    key: "session_cookie_name",
-    secret: "session_cookie_secret",
+    key: SESSION_COOKIE_NAME,
+    secret: SESSION_COOKIE_SECRET,
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
@@ -30,10 +42,10 @@ app.use(
 );
 
 // routes
-require("./routes")(app);
+require('./routes')(app);
 
 // post-route middlewares
-app.set("x-powered-by", false);
+app.set('x-powered-by', false);
 
 // server setup
 const server = app.listen(SERVER_PORT, () => {
@@ -43,15 +55,15 @@ const server = app.listen(SERVER_PORT, () => {
 });
 
 // process setup
-process.on("unhandledRejection", (error) => {
-  console.error("unhandledRejection", JSON.stringify(error), error.stack);
+process.on('unhandledRejection', (error) => {
+  console.error('unhandledRejection', JSON.stringify(error), error.stack);
   process.exit(1);
 });
-process.on("uncaughtException", (error) => {
-  console.error("uncaughtException", JSON.stringify(error), error.stack);
+process.on('uncaughtException', (error) => {
+  console.error('uncaughtException', JSON.stringify(error), error.stack);
   process.exit(1);
 });
-process.on("beforeExit", () => {
+process.on('beforeExit', () => {
   app.close((error) => {
     if (error) console.error(JSON.stringify(error), error.stack);
   });

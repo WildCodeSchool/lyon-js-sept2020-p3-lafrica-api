381c60d08f1f0b01b28a518fce8b93aa8f1523ff
Guillem <guillem.dardill@gmail.com>
2020-12-17 12:02:33 +0100

    front-back connexion added

--- middlewares/requireCurrentUser.js
+++ middlewares/requireCurrentUser.js
@@ -3,6 +3,9 @@ const userModel = require('../models/users.js');
 module.exports = async (req, res, next) => {
   const user = await userModel.findOne(req.session.userId, false);
   if (user && parseInt(req.params.user_id, 10) === user.id) {
+    console.log(user.id);
+    console.log(req.params.user_id);
+
     delete user.encrypted_password;
     req.currentUser = user;
     return next();

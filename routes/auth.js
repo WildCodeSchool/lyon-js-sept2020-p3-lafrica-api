063af6f51f3e2728e302e772b58617d847a32f33
cleroy117 <ce.leroy69@gmail.com>
2020-12-18 11:41:17 +0100

    joiForm

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const authController = require('../controllers/auth');
-const requireRequestBody = require('../middlewares/requireRequestBody');
+const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const authController = require("../controllers/auth");
+const requireRequestBody = require("../middlewares/requireRequestBody");
+const joiValidator = require("../middlewares/joiValidator");
 
-router.post('/login', requireRequestBody, asyncHandler(authController.login));
-router.get('/logout', asyncHandler(authController.logout));
+router.post(
+  "/login",
+  requireRequestBody,
+  joiValidator,
+  asyncHandler(authController.login)
+);
+router.get("/logout", asyncHandler(authController.logout));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-const userRouter = require('express').Router();
+const userRouter = require("express").Router();
 
-const asyncHandler = require('express-async-handler');
-const userController = require('../controllers/users');
+const asyncHandler = require("express-async-handler");
+const userController = require("../controllers/users");
+const joiValidator = require("../middlewares/joiValidator");
 
-userRouter.post('/signUp', asyncHandler(userController.handlePost));
+userRouter.post(
+  "/signUp",
+  joiValidator,
+  asyncHandler(userController.handlePost)
+);
 // userRouter.get('/:user_id', requireCurrentUser, asyncHandler());
 // pourra servir à afficher une page de l'utilisateur pour récupérer ses infos et les modifier (ROUTE + PUT MODEL)
 

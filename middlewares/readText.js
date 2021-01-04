22b4de126621a0ae0ea1bfe6cd419d7d5c43634d
cleroy117 <ce.leroy69@gmail.com>
2021-01-04 16:55:39 +0100

    errors handling

--- middlewares/readText.js
+++ middlewares/readText.js
@@ -3,7 +3,12 @@ const util = require("util");
 
 module.exports = async (req, res, next) => {
   const readfile = util.promisify(fs.readFile);
-  const uploadedTextToVocalize = await readfile(req.file.path, "utf-8");
-  req.uploadedTextToVocalize = uploadedTextToVocalize;
-  return next();
+  try {
+    const uploadedTextToVocalize = await readfile(req.file.path, "utf-8");
+    req.uploadedTextToVocalize = uploadedTextToVocalize;
+    return next();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Something went wrong");
+  }
 };

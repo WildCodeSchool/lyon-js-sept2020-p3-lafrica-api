0f23294b82cdf4d877b1ab794ef01db7fe2f6713
cleroy117 <ce.leroy69@gmail.com>
2021-01-12 15:25:03 +0100

    Route modify contact on work

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,8 +1,15 @@
-const { findAllContacts, createContacts } = require("../models/contacts");
+const {
+  findAllContacts,
+  createContacts,
+  modifyContact,
+} = require("../models/contacts");
 
 module.exports.getCollection = async (req, res) => {
   const data = await findAllContacts(req.currentUser.id);
-  res.json(data);
+  if (data) {
+    return res.status(200).json(data);
+  }
+  return res.status(400).send(`Impossible d'afficher les contacts`);
 };
 
 module.exports.createContacts = async (req, res) => {
@@ -11,4 +18,12 @@ module.exports.createContacts = async (req, res) => {
     return res.status(201).json(data);
   }
   return res.status(400).send(`Impossible d'ajouter de nouveaux contacts`);
+};
+
+module.exports.modifyContact = async (req, res) => {
+  const data = await modifyContact(req.body, req.params.id_contact);
+  if (data) {
+    return res.status(200).json(data);
+  }
+  return res.status(400).send(`Impossible de modifier le contact`);
 };
--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,30 @@
 const db = require("../db");
 
+/* const findOneContact = async (contactId) => {
+  const contact = db.query
+    .query("SELECT * FROM contact WHERE id = ?", [contactId])
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+  if (contact) {
+    return contact;
+  }
+  return null;
+}; */
+
 module.exports.findAllContacts = (id) => {
   return db.query("SELECT * FROM contact WHERE id_client_user  = ?", [id]);
 };
 
-module.exports.createContacts = async (newContacts, id) => {
+module.exports.createContacts = async (newContacts, currentUserId) => {
   const createdContacts = await Promise.all(
     newContacts.map(async (contact) => {
       const { lastname, firstname, phone_number } = contact;
       const result = await db
         .query(
           `INSERT INTO contact (lastname, firstname, phone_number, id_client_user) VALUES (?, ?, ?, ?)`,
-          [lastname, firstname, phone_number, id]
+          [lastname, firstname, phone_number, currentUserId]
         )
         .catch((err) => {
           console.log(err);
@@ -23,7 +36,7 @@ module.exports.createContacts = async (newContacts, id) => {
           lastname,
           firstname,
           phone_number,
-          id_client_user: id,
+          id_client_user: currentUserId,
         };
       }
       return null;
@@ -34,7 +47,19 @@ module.exports.createContacts = async (newContacts, id) => {
   }
   return null;
 };
-/* module.exports.modifyContact
 
-module.exports.deleteContact
- */
+module.exports.modifyContact = async (newAtttributes, contactId) => {
+  const { lastname, firstname, phone_number } = newAtttributes;
+  console.log(newAtttributes);
+  console.log(contactId);
+  await db
+    .query(
+      "UPDATE * FROM contact SET lastname = ?, firstname = ?, phone_number = ? WHERE id = ?",
+      [lastname, firstname, phone_number, contactId]
+    )
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+  /* findOneContact(contactId) */
+};

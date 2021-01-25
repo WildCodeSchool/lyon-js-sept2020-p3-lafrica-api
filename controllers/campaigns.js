b8d007e42fc96cabae108d3f5f46530d3e1bccbe
cleroy117 <ce.leroy69@gmail.com>
2021-01-25 15:38:26 +0100

    data export WIP

--- controllers/campaigns.js
+++ controllers/campaigns.js
@@ -109,3 +109,103 @@ module.exports.updateCampaign = async (req, res) => {
     .status(500)
     .send("Something went wrong uploading campaigns database");
 };
+
+module.exports.exportStatistics = async (req, res) => {
+  const data = [
+    {
+      firstName: "John",
+      lastName: "Bailey",
+      purchasePrice: 1000,
+      paymentsMade: 100,
+    },
+    {
+      firstName: "Leonard",
+      lastName: "Clark",
+      purchasePrice: 1000,
+      paymentsMade: 150,
+    },
+    {
+      firstName: "Phil",
+      lastName: "Knox",
+      purchasePrice: 1000,
+      paymentsMade: 200,
+    },
+    {
+      firstName: "Sonia",
+      lastName: "Glover",
+      purchasePrice: 1000,
+      paymentsMade: 250,
+    },
+    {
+      firstName: "Adam",
+      lastName: "Mackay",
+      purchasePrice: 1000,
+      paymentsMade: 350,
+    },
+    {
+      firstName: "Lisa",
+      lastName: "Ogden",
+      purchasePrice: 1000,
+      paymentsMade: 400,
+    },
+    {
+      firstName: "Elizabeth",
+      lastName: "Murray",
+      purchasePrice: 1000,
+      paymentsMade: 500,
+    },
+    {
+      firstName: "Caroline",
+      lastName: "Jackson",
+      purchasePrice: 1000,
+      paymentsMade: 350,
+    },
+    {
+      firstName: "Kylie",
+      lastName: "James",
+      purchasePrice: 1000,
+      paymentsMade: 900,
+    },
+    {
+      firstName: "Harry",
+      lastName: "Peake",
+      purchasePrice: 1000,
+      paymentsMade: 1000,
+    },
+  ];
+  // need to create a workbook object. Almost everything in ExcelJS is based off of the workbook object.
+  const workbook = 1; /* new Excel.Workbook(); */
+  const worksheet = workbook.addWorksheet("Debtors");
+  worksheet.columns = [
+    { header: "First Name", key: "firstName" },
+    { header: "Last Name", key: "lastName" },
+    { header: "Purchase Price", key: "purchasePrice" },
+    { header: "Payments Made", key: "paymentsMade" },
+    { header: "Amount Remaining", key: "amountRemaining" },
+    { header: "% Remaining", key: "percentRemaining" },
+  ];
+  // force the columns to be at least as long as their header row.
+  // Have to take this approach because ExcelJS doesn't have an autofit property.
+  worksheet.columns.forEach((column) => {
+    // eslint-disable-next-line no-param-reassign
+    column.width = column.header.length < 12 ? 12 : column.header.length;
+  });
+  // Make the header bold.
+  // Note: in Excel the rows are 1 based, meaning the first row is 1 instead of 0.
+  worksheet.getRow(1).font = { bold: true };
+  // Dump all the data into Excel
+  data.forEach((e) => {
+    // row 1 is the header.
+    // By using destructuring we can easily dump all of the data into the row without doing much
+    // We can add formulas pretty easily by providing the formula property.
+    worksheet.addRow({
+      ...e,
+    });
+  });
+  // Keep in mind that reading and writing is promise based.
+  await workbook.xlsx.writeFile("Debtors.xlsx");
+  const myFile = "Debtors.xlsx";
+  const pathFile = path.join(`${__dirname}/../${myFile}`);
+  /* res.download(pathFile); */
+  res.download(pathFile);
+};
--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,33 +1,38 @@
-const campaignsRouter = require('express').Router();
+const campaignsRouter = require("express").Router();
 
-const asyncHandler = require('express-async-handler');
-const campaignsController = require('../controllers/campaigns');
-const handleTextUpload = require('../middlewares/handleTextUpload');
+const asyncHandler = require("express-async-handler");
+const campaignsController = require("../controllers/campaigns");
+const handleTextUpload = require("../middlewares/handleTextUpload");
 
-campaignsRouter.get('/', asyncHandler(campaignsController.getCollection));
-campaignsRouter.post('/', asyncHandler(campaignsController.createCampaignId));
+campaignsRouter.get("/", asyncHandler(campaignsController.getCollection));
+campaignsRouter.post("/", asyncHandler(campaignsController.createCampaignId));
 
 campaignsRouter.put(
-  '/:campaignId',
+  "/:campaignId",
   asyncHandler(campaignsController.updateCampaign)
 );
 
 campaignsRouter.post(
-  '/uploadtext',
+  "/uploadtext",
   handleTextUpload,
   asyncHandler(campaignsController.readText)
 );
 
-campaignsRouter.post('/TTS', asyncHandler(campaignsController.vocalization));
-campaignsRouter.get('/audio', asyncHandler(campaignsController.playAudio));
+campaignsRouter.post("/TTS", asyncHandler(campaignsController.vocalization));
+campaignsRouter.get("/audio", asyncHandler(campaignsController.playAudio));
 campaignsRouter.get(
-  '/downloadaudio',
+  "/downloadaudio",
   asyncHandler(campaignsController.downloadAudio)
 );
 
 campaignsRouter.get(
-  '/:campaignId',
+  "/:campaignId",
   asyncHandler(campaignsController.getOneCampaign)
+);
+
+campaignsRouter.get(
+  "/:campaignId/exportStatistics",
+  asyncHandler(campaignsController.exportStatistics)
 );
 
 module.exports = campaignsRouter;

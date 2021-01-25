2c31b52b9a7ff8ff4002bbb05d96093f80c19692
Guillem <guillem.dardill@gmail.com>
2021-01-25 23:04:27 +0100

    statistics handleling from LAM API

--- scripts/campaignSendingDateCheck.js
+++ scripts/campaignSendingDateCheck.js
@@ -10,14 +10,28 @@ module.exports.campaignSendingDateCheck = async () => {
   if (!serviceIsRunning) {
     serviceIsRunning = true;
     const allCampaignsList = await db.query(
-      'SELECT * from mailing_campaign WHERE sending_status = 0'
+      'SELECT * from mailing_campaign WHERE sending_status = 0 OR sending_status = 1'
     );
+
     if (allCampaignsList.length === 0) {
       return null;
     }
     for (let i = 0; i < allCampaignsList.length; i += 1) {
       // Date and hour initialization
       const dateNow = new Date();
+
+      // Get list of campaigns that are not sent yet and for which date is higher that this routine date - put status "en attente"
+      if (
+        allCampaignsList[i].name &&
+        allCampaignsList[i].text_message &&
+        allCampaignsList[i].vocal_message_file_url &&
+        allCampaignsList[i].date
+      ) {
+        db.query(
+          'UPDATE mailing_campaign SET sending_status = 1 WHERE id = ?',
+          [allCampaignsList[i].id]
+        );
+      }
 
       // Get list of campaigns that are not sent yet and for which date is lower that this routine date
       if (
@@ -75,11 +89,34 @@ module.exports.campaignSendingDateCheck = async () => {
                 .then((res2) => {
                   if (res2) {
                     db.query(
-                      'UPDATE mailing_campaign SET sending_status = 1 WHERE id = ?',
+                      'UPDATE mailing_campaign SET sending_status = 2 WHERE id = ?',
                       [allCampaignsList[i].id]
                     );
+                    db.query(
+                      'UPDATE mailing_campaign SET lam_campaign_id = ? WHERE id = ?',
+                      [res2.data.id, allCampaignsList[i].id]
+                    );
+                    res2.data.calls.forEach((call) => {
+                      const formatedPhoneNumber = call.called.slice(2);
+                      db.query(
+                        'UPDATE contact_in_mailing_campaign AS jointure JOIN contact ON jointure.contact_id = contact.id JOIN mailing_campaign ON mailing_campaign.id = jointure.mailing_campaign_id SET lam_contact_id = ? WHERE contact.phone_number = ? AND mailing_campaign.lam_campaign_id = ?',
+                        [call.contactId, formatedPhoneNumber, res2.data.id]
+                      );
+                    });
                   }
                   console.log('retour API LAM', res2.data);
+                  // res.data.id = numéro de la campagne côté LAM
+                  // res.data.calls = tableau rassemblant tous les appels
+                  // res.data.calls[i] =
+                  //        id = numéro de l'appel côté LAM
+                  //        contactId = numéro du contact côté LAM
+                  //        called = numéro de téléphone appelé
+                  //        callStateId: 1, ???
+                  //        callResultId: 1, ???
+                  //        processingCallCount: 0,
+                  // callSuccessCount: 0,
+                  // callFailedCount: 0,
+                  // callIgnoredCount: 0,
                 });
             }, i * 1000);
           });
@@ -89,3 +126,22 @@ module.exports.campaignSendingDateCheck = async () => {
   }
   return 'campaings list to send checked';
 };
+
+// module.exports.campaignHistory = async () => {
+//   let serviceIsRunning = false;
+//   if (!serviceIsRunning) {
+//     serviceIsRunning = true;
+//     const allContactToUpdate = await db.query(
+//       'SELECT * from contact_in_mailing_campaign JOIN WHERE callStatedId = 1'
+//     );
+//     if (allContactToUpdate.length === 0) {
+//       return null;
+//     }
+//     const callHistory = await db.query(
+//       'SELECT * from contact_in_mailing_campaign WHERE callStatedId = 1'
+//     );
+
+//     for (let i = 0; i < allContactToUpdate.length; i += 1) {
+
+//   }
+// };

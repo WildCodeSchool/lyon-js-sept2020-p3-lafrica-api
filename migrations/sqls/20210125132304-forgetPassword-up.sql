f66e61da974238b0a3dbc62ea8b4dd014749f246
Jean Ducret <jean.dcrt@gmail.com>
2021-01-26 09:16:00 +0100

    add migration

--- migrations/sqls/20210125132304-forgetPassword-up.sql
+++ migrations/sqls/20210125132304-forgetPassword-up.sql
@@ -8,3 +8,23 @@ ADD
   COLUMN resetPasswordExpires datetime;
 ALTER TABLE
   mailing_campaign CHANGE COLUMN sending_status sending_status INT NOT NULL DEFAULT '0';
+ALTER TABLE
+  mailing_campaign
+ADD
+  COLUMN lam_campaign_id INT NULL DEFAULT NULL
+AFTER
+  sending_status;
+ALTER TABLE
+  contact_in_mailing_campaign
+ADD
+  COLUMN lam_contact_id INT NULL DEFAULT NULL
+AFTER
+  sending_status,
+ADD
+  COLUMN call_state_id INT NULL DEFAULT NULL
+AFTER
+  lam_contact_id,
+ADD
+  COLUMN call_result_id INT NULL DEFAULT NULL
+AFTER
+  call_state_id;
